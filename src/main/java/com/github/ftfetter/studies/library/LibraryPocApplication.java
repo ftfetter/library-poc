4a5968c103160876597c8971c21090e130863b44
felipe.fetter <ilegra.felipef@agibank.com.br>
2018-08-09 00:37:07 -0300

    Initial commit

---
+++ src/main/java/com/github/ftfetter/studies/library/LibraryPocApplication.java
@@ -0,0 +1,12 @@
+package com.github.ftfetter.studies.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPocApplication.class, args);
+	}
+}
